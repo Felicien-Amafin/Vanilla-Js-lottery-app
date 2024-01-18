e996fee23b98197d00c433bb84623ddd414f1582
Felicien Amafin <felicien.amafin@gmail.com>
2024-01-18 14:42:33 +0100

    Applied style to header

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const clean = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'src/main.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[contenthash].js',
+    },
+    devtool: 'source-map',
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist')
+        },
+        port: 3000,
+        open: true,
+        hot: true,
+        liveReload: true,
+        compress: true,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Euromillions Results',
+            filename: 'index.html',
+            template: './src/template.html'
+        }),
+        new clean.CleanWebpackPlugin()
+    ]
+}
